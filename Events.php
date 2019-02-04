48c8d7d093b552ff376791bf568abf4b4052e17d
Lars Stian <larsstianf@outlook.com>
2019-02-04 16:48:11 +0100

    La til events page + bilde

---
+++ Events.php
@@ -0,0 +1,36 @@
+<?php
+include 'rsc/imports/php/head.php';
+include 'rsc/imports/php/header.php';
+
+?>
+
+<!--
+##################################################################################
+######################## ! DO NOT EDIT ABOVE THIS POINT ! ########################
+##################################################################################
+-->
+
+<main>
+
+    <section>
+        <div class="main-picture">
+
+            <!-- logo navbar start -->
+            <a href="events" class=""><img src="rsc/img/events.jpeg"></a>
+            <!-- logo navbar stop -->
+
+        </div>
+
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+</main>
