5cd241e5b87e4071dac60eb8450b81b5c379b0cd
BertBert0 <31742102+BertBert0@users.noreply.github.com>
2019-03-28 10:31:43 +0100

    Navbar new

--- admin/rsc/imports/php/components/admin_header.php
+++ admin/rsc/imports/php/components/admin_header.php
@@ -3,48 +3,122 @@
     :root{ --navbar-height: 9rem; }
     nav ~ main {margin-top: var(--navbar-height);}
 </style>
-<script src="https://code.jquery.com/jquery-1.12.0.js"></script>
-<script src="..\rsc\imports\js\navjs.js"></script>
+
+
+
+
+
+<!------ Include the above in your HEAD tag ---------->
+<link rel="stylesheet" href="../rsc/imports/css/custom.css">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- Navigation -->
+<div class="">
+    <header class="topbar">
+        <div class="container">
+            <div class="row">
+                <!-- social icon-->
+                <div class="col-sm-12">
+                    <ul class="social-network">
+                        <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-home"></i></a></li>
+                        <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-sign-out"></i></a></li>
+
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark mx-background-top-linear">
+        <div class="container">
+            <a class="navbar-brand" href="index.html"><img class="" src="../rsc/img/kroa-logo-text.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                <ul class="navbar-nav ml-auto">
+
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Event</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">People</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Manage</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Unitlist</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+
+
+
+
+<!--
 <nav class="fixed-top">
 
-    <div id="customnav" class="navbar navbar-expand-sm">
-        <img id="navimg" src="../rsc/img/navhome1.png">
 
-        <a class="navbar-brand"><img src="../rsc/img/Kroalogo.png" alt="logo" style="width:40px;"></a>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link mainnav" id="homenav" href="#">Home</a>
-            <li class="nav-item ">
-                <a class="nav-link mainnav" id="eventnav" href="#">Events</a>
-            </li>
-            <li class="nav-item ">
-                <a class="nav-link mainnav" id="peoplenav" href="#">People</a>
-            </li>
-        </ul>
-    </div>
+      ##################################################################################
+   ######################## ! OLD FANCY NAV - INCOMPLETE ! ########################
+   ##################################################################################
+   <script src="https://code.jquery.com/jquery-1.12.0.js"></script>
+   <script src="..\rsc\imports\js\navjs.js"></script>
 
-    <div id="secondnav" class="navbar navbar-expand-sm" >
+   <div id="customnav" class="navbar navbar-expand-sm">
+       <img id="navimg" src="../rsc/img/navhome1.png">
 
-        <div class="subhide" id="homenavsub">
-            <a class="nav-link" href="#">Home</a>
-            <a class="nav-link" href="#">Home</a>
-            <a class="nav-link" href="#">Home</a>
-            <a class="nav-link" href="#">Home</a>
+       <a class="navbar-brand"><img src="../rsc/img/Kroalogo.png" alt="logo" style="width:40px;"></a>
+       <ul class="navbar-nav">
+           <li class="nav-item">
+               <a class="nav-link mainnav" id="homenav" href="#">Home</a>
+           <li class="nav-item ">
+               <a class="nav-link mainnav" id="eventnav" href="#">Events</a>
+           </li>
+           <li class="nav-item ">
+               <a class="nav-link mainnav" id="peoplenav" href="#">People</a>
+           </li>
+       </ul>
+   </div>
 
-        </div>
+   <div id="secondnav" class="navbar navbar-expand-sm" >
 
-        <div class="subhide" id="eventnavsub">
-            <a class="nav-link" href="#">Event</a>
-            <a class="nav-link" href="#">Event</a>
-            <a class="nav-link" href="#">Event</a>
-            <a class="nav-link" href="#">Event</a>
-        </div>
+       <div class="subhide" id="homenavsub">
+           <a class="nav-link" href="#">Home</a>
+           <a class="nav-link" href="#">Home</a>
+           <a class="nav-link" href="#">Home</a>
+           <a class="nav-link" href="#">Home</a>
 
-        <div class="subhide" id="peoplenavsub">
-            <a class="nav-link" href="#">People</a>
-            <a class="nav-link" href="#">People</a>
-            <a class="nav-link" href="#">People</a>
-            <a class="nav-link" href="#">People</a>
-        </div>
-    </div>
+       </div>
+
+       <div class="subhide" id="eventnavsub">
+           <a class="nav-link" href="#">Event</a>
+           <a class="nav-link" href="#">Event</a>
+           <a class="nav-link" href="#">Event</a>
+           <a class="nav-link" href="#">Event</a>
+       </div>
+
+       <div class="subhide" id="peoplenavsub">
+           <a class="nav-link" href="#">People</a>
+           <a class="nav-link" href="#">People</a>
+           <a class="nav-link" href="#">People</a>
+           <a class="nav-link" href="#">People</a>
+       </div>
+   </div>
+
 </nav>
+-->
