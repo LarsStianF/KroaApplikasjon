6f6be9095d157afc8545b5197c09c5e62f14e13b
KimPedersen91 <32094155+KimPedersen91@users.noreply.github.com>
2019-04-02 11:23:13 +0200

    fixx in events cards

--- admin/events.php
+++ admin/events.php
@@ -16,10 +16,14 @@ include 'rsc/imports/php/components/admin_header.php';
         <div class="row pt-3">
             <div class="card card-custom mx-2 mb-3 border-light">
 
-                <img class="card-img-top" src="../rsc/img/Openscenecut.png" alt="">
+                <div class="card-img-caption">
+                    <p class="card-text-top">ARRANGEMENT TITTEL</p>
+                    <p class="card-text-bottom">ARRANGEMENT DATO</p>
+                    <img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">
+                </div>
+
 
                 <div class="card-body d-flex flex-column">
-                    <h5 class="card-title text-center">Interessant Tittel</h5>
                     <h6 class="card-subtitle text-left pt-2">Gripende undertekst</h6>
                     <h6 class="card-subtitle my-2 text-muted text-left">test</h6>
 
@@ -32,17 +36,20 @@ include 'rsc/imports/php/components/admin_header.php';
                         <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_blue.png" alt=""></li>
                         <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
                     </ul>
-                    <button class="btn btn-primary">Click me!</button>
+                    <button class="btn btn-primary mt-2">Click me!</button>
                 </div>
 
             </div>
-
             <div class="card card-custom mx-2 mb-3 border-light">
 
-                <img class="card-img-top" src="../rsc/img/Openscenecut.png" alt="">
+                <div class="card-img-caption">
+                    <p class="card-text-top">ARRANGEMENT TITTEL</p>
+                    <p class="card-text-bottom">ARRANGEMENT DATO</p>
+                    <img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">
+                </div>
+
 
                 <div class="card-body d-flex flex-column">
-                    <h5 class="card-title text-center">Interessant Tittel</h5>
                     <h6 class="card-subtitle text-left pt-2">Gripende undertekst</h6>
                     <h6 class="card-subtitle my-2 text-muted text-left">test</h6>
 
@@ -55,17 +62,20 @@ include 'rsc/imports/php/components/admin_header.php';
                         <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_blue.png" alt=""></li>
                         <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
                     </ul>
-                    <button class="btn btn-primary">Click me!</button>
+                    <button class="btn btn-primary mt-2">Click me!</button>
                 </div>
 
             </div>
-
             <div class="card card-custom mx-2 mb-3 border-light">
 
-                <img class="card-img-top" src="../rsc/img/Openscenecut.png" alt="">
+                <div class="card-img-caption">
+                    <p class="card-text-top">ARRANGEMENT TITTEL</p>
+                    <p class="card-text-bottom">ARRANGEMENT DATO</p>
+                    <img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">
+                </div>
+
 
                 <div class="card-body d-flex flex-column">
-                    <h5 class="card-title text-center">Interessant Tittel</h5>
                     <h6 class="card-subtitle text-left pt-2">Gripende undertekst</h6>
                     <h6 class="card-subtitle my-2 text-muted text-left">test</h6>
 
@@ -78,10 +88,92 @@ include 'rsc/imports/php/components/admin_header.php';
                         <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_blue.png" alt=""></li>
                         <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
                     </ul>
-                    <button class="btn btn-primary">Click me!</button>
+                    <button class="btn btn-primary mt-2">Click me!</button>
                 </div>
 
             </div>
+
+        </div>
+        <div class="row pt-3">
+            <div class="card card-custom mx-2 mb-3 border-light">
+
+                <div class="card-img-caption">
+                    <p class="card-text-top">ARRANGEMENT TITTEL</p>
+                    <p class="card-text-bottom">ARRANGEMENT DATO</p>
+                    <img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">
+                </div>
+
+
+                <div class="card-body d-flex flex-column">
+                    <h6 class="card-subtitle text-left pt-2">Gripende undertekst</h6>
+                    <h6 class="card-subtitle my-2 text-muted text-left">test</h6>
+
+                    <div class="text-left">
+                        <p class="card-text">Her kan det stå litt infomarjon, kanskje et utdrag who knows? test deg frem eller..</p>
+                    </div>
+                    <ul class="list-group-flush  volunteers_list">
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_red.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_blue.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
+                    </ul>
+                    <button class="btn btn-primary mt-2">Click me!</button>
+                </div>
+
+            </div>
+            <div class="card card-custom mx-2 mb-3 border-light">
+
+                <div class="card-img-caption">
+                    <p class="card-text-top">ARRANGEMENT TITTEL</p>
+                    <p class="card-text-bottom">ARRANGEMENT DATO</p>
+                    <img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">
+                </div>
+
+
+                <div class="card-body d-flex flex-column">
+                    <h6 class="card-subtitle text-left pt-2">Gripende undertekst</h6>
+                    <h6 class="card-subtitle my-2 text-muted text-left">test</h6>
+
+                    <div class="text-left">
+                        <p class="card-text">Her kan det stå litt infomarjon, kanskje et utdrag who knows? test deg frem eller..</p>
+                    </div>
+                    <ul class="list-group-flush  volunteers_list">
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_red.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_blue.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
+                    </ul>
+                    <button class="btn btn-primary mt-2">Click me!</button>
+                </div>
+
+            </div>
+            <div class="card card-custom mx-2 mb-3 border-light">
+
+                <div class="card-img-caption">
+                    <p class="card-text-top">ARRANGEMENT TITTEL</p>
+                    <p class="card-text-bottom">ARRANGEMENT DATO</p>
+                    <img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">
+                </div>
+
+
+                <div class="card-body d-flex flex-column">
+                    <h6 class="card-subtitle text-left pt-2">Gripende undertekst</h6>
+                    <h6 class="card-subtitle my-2 text-muted text-left">test</h6>
+
+                    <div class="text-left">
+                        <p class="card-text">Her kan det stå litt infomarjon, kanskje et utdrag who knows? test deg frem eller..</p>
+                    </div>
+                    <ul class="list-group-flush  volunteers_list">
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_red.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_blue.png" alt=""></li>
+                        <li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>
+                    </ul>
+                    <button class="btn btn-primary mt-2">Click me!</button>
+                </div>
+
+            </div>
+
         </div>
 
     </section>
