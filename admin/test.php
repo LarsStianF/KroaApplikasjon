2ae6893ac5a8d53e7ada773ec0145c566c3ef818
BertBert0 <31742102+BertBert0@users.noreply.github.com>
2019-04-04 14:06:18 +0200

    Meta tag

---
+++ admin/test.php
@@ -0,0 +1,195 @@
+<?php
+include 'rsc/imports/php/components/admin_head.php';
+include 'rsc/imports/php/components/admin_header.php';
+
+?>
+
+    <!--
+    ##################################################################################
+    ######################## ! DO NOT EDIT ABOVE THIS POINT ! ########################
+    ##################################################################################
+    -->
+
+
+
+
+
+
+
+
+    <main>
+
+        <section class="container pt-2">
+            <div class="row mt-5">
+
+                <section class="container py-5">
+
+                    <div class="row">
+                        <div class="col text-center">
+
+                            <section class="container pt-2">
+
+
+
+                                <div class="row mt-3 ">
+                                    <div class="col-md-8 themed-grid-col">
+                                        <div class="pb-3">
+                                            <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-warning rounded shadow-sm">
+                                                <img class="mr-3" src="../rsc/img/userpic.jpg" alt="" width="48" height="48">
+                                                <div class="lh-100">
+                                                    <h6 class="mb-0 text-white lh-100">Welcome, Name Nameson</h6>
+                                                    <small>Last login: 22 January, 2019</small>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6 themed-grid-col"> <div class="my-3 p-3 bg-white rounded shadow-sm">
+                                                    <h6 class="border-bottom border-gray pb-2 mb-0">Your confirmed upcoming events</h6>
+                                                    <div class="media text-muted pt-3">
+                                                        <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#007bff" width="100%" height="100%"/><text fill="#007bff" dy=".3em" x="50%" y="50%">32x32</text></svg>
+                                                        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                                            <strong class="d-block text-gray-dark">Kroa valget 2019</strong>
+                                                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                                        </p>
+                                                    </div>
+                                                    <div class="media text-muted pt-3">
+                                                        <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#e83e8c" width="100%" height="100%"/><text fill="#e83e8c" dy=".3em" x="50%" y="50%">32x32</text></svg>
+                                                        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                                            <strong class="d-block text-gray-dark h4">Loveshack</strong>
+                                                            <strong class="d-block text-gray dark">Date: 26.2.2019 Time: 20.00 - 02.30</strong>
+                                                            <strong class="d-block text-gray dark">Meetup: kl 18.00</strong>
+                                                            <strong class="d-block text-gray dark">You are working as: Bar</strong>
+                                                            <strong class="d-block text-gray dark">6/6 Bar, 4/5 Teknisk, 4/4 Crew, 10/14 Security</strong>
+
+                                                            <span class="text-gray dark h6">Additional Notes: </span>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                                        </p>
+                                                    </div>
+                                                    <div class="media text-muted pt-3">
+                                                        <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#6f42c1" width="100%" height="100%"/><text fill="#6f42c1" dy=".3em" x="50%" y="50%">32x32</text></svg>
+                                                        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                                            <strong class="d-block text-gray-dark">Åpen Scene</strong>
+                                                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                                        </p>
+                                                    </div>
+                                                    <small class="d-block text-right mt-3">
+                                                        <a href="#">Sign up for more events!</a>
+                                                    </small>
+                                                </div></div>
+                                            <div class="col-md-6 themed-grid-col"> <div class="my-3 p-3 bg-white rounded shadow-sm">
+                                                    <h6 class="border-bottom border-gray pb-2 mb-0">Your signed upcoming events</h6>
+                                                    <div class="media text-muted pt-3">
+                                                        <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#007bff" width="100%" height="100%"/><text fill="#007bff" dy=".3em" x="50%" y="50%">32x32</text></svg>
+                                                        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                                            <strong class="d-block text-gray-dark">Karpe</strong>
+                                                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                                        </p>
+                                                    </div>
+                                                    <div class="media text-muted pt-3">
+                                                        <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#e83e8c" width="100%" height="100%"/><text fill="#e83e8c" dy=".3em" x="50%" y="50%">32x32</text></svg>
+                                                        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                                            <strong class="d-block text-gray-dark">Rotlaus</strong>
+                                                            <strong class="d-block text-gray dark">Date: 26.2.2019 Time: 20.00 - 02.30</strong>
+                                                            <strong class="d-block text-gray dark">Meetup: kl 18.00</strong>
+                                                            <strong class="d-block text-gray dark">You are signed as: Teknisk, Security</strong>
+                                                            <strong class="d-block text-gray dark">6/6 Bar, 4/5 Teknisk, 4/4 Crew, 10/14 Security</strong>
+                                                            <span class="text-gray dark h6">Additional Notes: </span>
+                                                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                                        </p>
+                                                    </div>
+                                                    <div class="media text-muted pt-3">
+                                                        <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect fill="#6f42c1" width="100%" height="100%"/><text fill="#6f42c1" dy=".3em" x="50%" y="50%">32x32</text></svg>
+                                                        <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                                                            <strong class="d-block text-gray-dark">X-RUSS</strong>
+                                                            Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+                                                        </p>
+                                                    </div>
+                                                    <small class="d-block text-right mt-3">
+                                                        <a href="#">Sign up for more events!</a>
+                                                    </small>
+                                                </div></div>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4 themed-grid-col">
+
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="stat-widget-one">
+                                                    <div class="stat-icon dib"><i class="ti-enhet text-success border-success"></i></div>
+                                                    <div class="stat-content dib">
+                                                        <div class="stat-text">Your free units</div>
+                                                        <div class="stat-digit">4</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="stat-widget-one">
+                                                    <div class="stat-icon dib"><i class="ti-total text-success border-success"></i></div>
+                                                    <div class="stat-content dib">
+                                                        <div class="stat-text">Times worked</div>
+                                                        <div class="stat-digit">42</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="stat-widget-one">
+                                                    <div class="stat-icon dib"><i class="ti-month text-success border-success"></i></div>
+                                                    <div class="stat-content dib">
+                                                        <div class="stat-text">Times worked this month</div>
+                                                        <div class="stat-digit">5</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <div class="stat-widget-one">
+                                                    <div class="stat-icon dib"><i class="ti-jippi text-success border-success"></i></div>
+                                                    <div class="stat-content dib">
+                                                        <div class="stat-text">You have JIPPI until</div>
+                                                        <div class="stat-digit">31 February</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+                                    </div>
+
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+
+                </section>
+            </div>
+        </section>
+    </main>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="/docs/4.2/assets/js/vendor/jquery-slim.min.js"><\/script>')</script><script src="/docs/4.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-zDnhMsjVZfS3hiP7oCBRmfjkQC4fzxVxFhBx8Hkz2aZX8gEvA/jsP3eXRCvzTofP" crossorigin="anonymous"></script>
+    <script src="offcanvas.js"></script></body>
+
+
+
+
+    <!--
+    ##################################################################################
+    ######################## ! DO NOT EDIT BELOW THIS POINT ! ########################
+    ##################################################################################
+    -->
+
+<?php include '../rsc/imports/php/components/footer.php'; ?>
