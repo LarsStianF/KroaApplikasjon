c410a88ce498a38769831c5b035f159f9ac261b2
BertBert0 <31742102+BertBert0@users.noreply.github.com>
2019-04-08 17:15:29 +0200

    Update unitlist.php

--- admin/unitlist.php
+++ admin/unitlist.php
@@ -12,6 +12,32 @@ include 'dbcon.php';
 
 
 <script>
+
+    $(document).ready(function(){
+        $('#unitSearch input[type="text"]').on("keyup input", function(){
+            /* Get input value on change */
+            var inputVal = $(this).val();
+            var resultDropdown = $(this).siblings(".result");
+            if(inputVal.length){
+                $.get(this, {term: inputVal}).done(function(data){
+                    // Display the returned data in browser
+                    resultDropdown.html(data);
+                });
+            } else{
+                resultDropdown.empty();
+            }
+        });
+
+        // Set search input value on click of result item
+        $(document).on("click", ".result p", function(){
+            $(this).parents("#unitSearch").find('input[type="text"]').val($(this).text());
+            $(this).parent(".result").empty();
+        });
+    });
+
+
+
+    /* *OLD SCRIPT *
     $(document).ready(function(){
         $("#unitSearch").on("keyup", function() {
             var value = $(this).val().toLowerCase();
@@ -20,6 +46,7 @@ include 'dbcon.php';
             });
         });
     });
+    /*
 </script>
 
 
@@ -42,30 +69,48 @@ include 'dbcon.php';
 
 
 <?php
-    $sql = 'SELECT * FROM volunteer';
-    $result = mysqli_query($con, $sql);
-    $numrow = mysqli_num_rows($result);
+if(isset($_REQUEST["term"])) {
+    $sql = 'SELECT * FROM volunteer WHERE Firstname LIKE ?';
+    if ($stmt = mysqli_prepare($con, $sql)) {
+        mysqli_stmt_bind_param($stmt, "s", $param_term);
+        $param_term = $_REQUEST["term"] . '%';
 
-echo '<div class="list-group" id="namerow">';
-while ( $row = mysqli_fetch_array( $result ) ) {
+        if (mysqli_stmt_execute($stmt)) {
+            $result = mysqli_stmt_get_result($stmt);
 
-    echo '<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">';
-    echo '<div class="d-flex w-100 justify-content-between">';
-    echo '<h2 class="mb-1" id="nameblock">';
-    echo $row['Firstname'] . ' ' . $row['Lastname'];
-    echo '</h2>';
-    echo '<h2 class="mb-1" >';
-    if ($row['Unit'] > 0) {
-        echo '<span class="badge badge-success"> Units: ';
-    }
-    else {
-        echo '<span class="badge badge-danger"> Units: ';
-    }
-    echo $row['Unit'];
-    echo '</span> </h2>';
-    echo '</div>';
-    echo '</a>';
+            if (mysqli_num_rows($result) > 0) {
+
+
+                echo '<div class="list-group" id="namerow">';
+                while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+
+                    echo '<a href="#url1" class="list-group-item list-group-item-action flex-column align-items-start">';
+                    echo '<div class="d-flex w-100 justify-content-between">';
+                    echo '<h2 class="mb-1" id="nameblock">';
+                    echo $row['Firstname'] . ' ' . $row['Lastname'];
+                    echo '</h2>';
+                    echo '<h2 class="mb-1" >';
+                    if ($row['Unit'] > 0) {
+                        echo '<span class="badge badge-success"> Units: ';
+                    } else {
+                        echo '<span class="badge badge-danger"> Units: ';
+                    }
+                    echo $row['Unit'];
+                    echo '</span> </h2>';
+                    echo '</div>';
+                    echo '</a>';
+                }
+            } else{
+                    echo "<p>No matches found</p>";
+                }
+            } else{
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($con);
+            }
+        }
+
+    mysqli_stmt_close($stmt);
 }
+mysqli_close($con);
 
 
 ?>
