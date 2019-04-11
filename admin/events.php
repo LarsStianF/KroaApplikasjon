f119b17382c681e1f740181df966235500642498
KimPedersen91 <32094155+KimPedersen91@users.noreply.github.com>
2019-04-11 18:46:59 +0200

    modal fix, function for workers check and data.

--- admin/events.php
+++ admin/events.php
@@ -3,96 +3,87 @@ include 'rsc/imports/php/components/admin_head.php';
 include 'rsc/imports/php/components/admin_header.php';
 include 'dbcon.php';
 include 'rsc/imports/modals/event_modal.php';
-
-
+include '../rsc/imports/php/functions/functions.php';
 ?>
 
-    <!--
-    ##################################################################################
-    ######################## ! DO NOT EDIT ABOVE THIS POINT ! ########################
-    ##################################################################################
-    -->
-    <main class="container">
+    <main>
+
+        <?php
+
+        $sql = 'SELECT * FROM event ORDER BY Date ASC, Time_Start ASC';
+        $result = mysqli_query($con, $sql);
+        $numrow = mysqli_num_rows($result);
+        $counter = 1;
+        echo '<section class="container pt-2">';
+        echo '<div class="row mt-5 ">';
+        while ( $row = mysqli_fetch_array( $result ) ){
+
+            // Gets date
+            $id = $row['ID'];
+            $d = new DateTime($row['Date']);
+            $date = $d->format('F jS o');
+
+            // Gets time
+            $sTime = new DateTime($row['Time_Start']);
+            $eTime = new DateTime($row['Time_End']);
+            $startTime = $sTime->format('H:i');
+            $endTime = $eTime->format('H:i');
+
+            echo '<div class="card card-custom mx-2 mb-4 border-dark">';
+            echo '<div class="card-img-caption">';
+            echo '<p class="card-text-top">'.$row['Name'].'</p>';
+            echo '<p class="card-text-bottom">'.$date.'</p>';
+            echo '<img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">';
+            echo '</div>';
+            echo '<div class="card-body d-flex flex-column">';
+            echo '<h5 class="card-title text-center">';
+            echo $startTime . ' - ' . $endTime;
+            echo '</h5>';
+            get_event_volunteers($id);
+            echo '<p class="card-text text-center">';
+            echo $row['Event_text'];
+            echo '</p>';
+            echo '<a href="#eventModal" value="view" type="button" name="view" class="btn btn-primary btn-small border-dark m-2 view_data" id="'.$id.'" data-toggle="modal" >Details</a>';
+            echo '</div>';
+            echo '</div>';
 
 
+            if ($counter % 3 == 0) {
 
-            <div class="row pt-3">
-                <?php
-                $query = "SELECT * FROM event ORDER BY Date DESC, ID DESC;";
-                $result = mysqli_query($con, $query);
-                $count = 1;
-                $numrow = mysqli_num_rows($result);
+                if ($counter == $numrow) {
+                    echo '</div>';
+                    echo '</section>';
+                } else {
 
-
-                while ($row = mysqli_fetch_array($result)) {
-
-
-                    echo  '<div class=" card card-custom justify-content mx-2 mb-3 border-light">';
-
-                    echo   '<div class="card-img-caption">';
-                    echo        '<p class="card-text-top">'.$row['Name'].'</p>';
-                    echo        '<p class="card-text-bottom">'.$row['Date'].'</p>';
-                    echo        '<img class="card-img-top" src="../rsc/img/eventbackground.png" alt="">';
-                    echo   '</div>';
-                    echo   '<div class="card-body d-flex flex-column">';
-                    echo        '<div class="text-center">';
-                    echo            '<p class="card-text">'.$row['Event_text'].'</p>';
-                    echo        '</div>';
-                    echo        '<ul class="list-group-flush  volunteers_list">';
-                    echo            '<li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>';
-                    echo            '<li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_red.png" alt=""></li>';
-                    echo            '<li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_blue.png" alt=""></li>';
-                    echo            '<li class="list-group-item volunteers_item"><p>6/10</p><img class="man-icon" src="../rsc/img/man_black.png" alt=""></li>';
-                    echo        '</ul>';
-                    echo    '<a href="#eventModal" value="view" type="button" name="view" class="btn btn-primary btn-small border-dark m-2 view_data" id="'.$row['ID'].'" data-toggle="modal" >Details</a>';
-                    echo   '</div>';
-                    echo  '</div>';
-
-
-                    if ($count % 3 == 0) {
-                        echo '</div>';
-                        if ($count == $numrow) {
-
-                        } else {
-                            echo '<div class="row pt-3">';
-                        }
-
-
-
-                    }
-                    $count++;
                 }
 
+            }
+            $counter++;
+        }
 
-                ?>
-                <script>
-                    $(document).ready(function () {
-                        $('.view_data').click(function () {
-                            var id = $(this).attr('id');
-
-                            $.ajax({
-                                url: "fetch_data.php",
-                                method: "post",
-                                data: {id:id},
-                                success:function (data) {
-                                    $('#event_detail').html(data);
-                                    $('#eventModal').modal("show");
-                                }
-                            });
+        ?>
+        <script>
 
 
-                        });
+            $(document).ready(function () {
+
+                $('.view_data').click(function () {
+                    var id = $(this).attr('id');
+
+                    $.ajax({
+                        url: "fetch_modal_data.php",
+                        method: "post",
+                        data: {id:id},
+                        success:function (data) {
+                            $('#event_detail').html(data);
+                            $('#eventModal').modal("show");
+                        }
                     });
-                </script>
+                });
+            });
 
+        </script>
 
     </main>
-
-
-    <!--
-    ##################################################################################
-    ######################## ! DO NOT EDIT BELOW THIS POINT ! ########################
-    ##################################################################################
-    -->
 
 <?php include '../rsc/imports/php/components/footer.php'; ?>
---
+++ admin/fetch_modal_data.php
@@ -0,0 +1,45 @@
+<?php
+include 'dbcon.php';
+include '../rsc/imports/php/functions/functions.php';
+
+
+if(isset($_POST['id']))
+{
+    $id = $_POST['id'];
+    $output = "";
+    $query = "SELECT * FROM event WHERE ID = '".$id."'";
+    $result =  mysqli_query($con, $query);
+    $output .= '
+    <div class="text-center">';
+        $row = mysqli_fetch_array($result);
+
+        //gets time
+        $sTime = new DateTime($row['Time_Start']);
+        $eTime = new DateTime($row['Time_End']);
+        $startTime = $sTime->format('H:i');
+        $endTime = $eTime->format('H:i');
+
+        // get fix date
+        $d = new DateTime($row['Date']);
+        $date = $d->format('d-m-Y');
+
+            $output .= '
+                <h1>'.$row['Name'].'</h1> 
+                <hr class="mt-1">
+                <h2>'.$date.'</h2>
+                <h2>'.$startTime.' - '.$endTime.'</h2>
+                <h2><label class="mr-2">Event type:</label>'.$row['Type'].'</h2>
+                <hr>
+                <h6>Info</h6>
+                <p>'.$row['Event_text'].'</p>
+            ';
+
+        $output .= "</div>";
+
+
+
+        echo $output;
+        get_event_modal_volunteers($id);
+
+}
+?>
