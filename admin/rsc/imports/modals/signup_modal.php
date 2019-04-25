06ac1b8311ad76b2bb497b6f7f5001f58bef0997
KimPedersen91 <32094155+KimPedersen91@users.noreply.github.com>
2019-04-25 21:22:31 +0200

    Signup feedback

---
+++ admin/rsc/imports/modals/signup_modal.php
@@ -0,0 +1,79 @@
+<?php
+if ( isset($_GET['signup']) ) {
+
+
+        $object = "user";
+
+    // Check if request is for a file:
+
+    if ($_GET['signup'] == 'invalid') {
+
+
+        $title_color = 'text-danger';
+        $status_adjective = 'unsuccessfully';
+        $title_text = 'Use of invalid characters';
+
+
+    } elseif ($_GET['signup'] == 'email') {
+        $title_color = 'text-danger';
+        $status_adjective = 'unsuccessfully';
+        $title_text = 'Email not entered correctly';
+
+
+    } elseif($_GET['signup'] == 'usertaken') {
+        $title_color = 'text-danger';
+        $status_adjective = 'unsuccessfully';
+        $title_text = 'User with that email exists';
+
+    } elseif($_GET['signup'] == 'success') {
+        $status_adjective = 'successfully';
+        $title_text = 'User created';
+        $title_color = 'text-success';
+    }
+
+    $modal = '
+    <!-- Executable link for modal window: -->
+    <a id="modal-run" class="hidden" data-toggle="modal" data-target="#modal-created"></a>
+
+    <!-- Modal window -->
+    <div class="modal fade" id="modal-created" tabindex="-1" role="dialog" aria-labelledby="modal-created-label" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title ' . $title_color . '"> ' . $title_text . '!</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body text-secondary">
+                    <p>The ' . $object . ' was ' . $status_adjective . ' Created.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Click executable link on page load: -->
+    <script>
+        window.onload=function(){
+            if(document.getElementById(\'modal-run\')!=null||document.getElementById(\'modal-run\')!=""){
+                document.getElementById(\'modal-run\').click();
+            }
+        }
+    <!-- Redirect page on modal close: -->
+    $(".modal").on("hidden.bs.modal", function () {
+    window.location = "index.php";
+    });
+    </script>
+    ';
+
+
+    // Echo modal:
+    echo $modal;
+}
+
+
+
+?>
--- rsc/imports/php/components/head.php
+++ rsc/imports/php/components/head.php
@@ -8,6 +8,6 @@
 
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     </head>
     <body>
