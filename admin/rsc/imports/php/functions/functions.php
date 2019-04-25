011bcc746a0084ce20fc31203a39ab3a2c4e98aa
KimPedersen91 <32094155+KimPedersen91@users.noreply.github.com>
2019-04-25 21:58:32 +0200

    tok bort testing

--- admin/rsc/imports/php/functions/functions.php
+++ admin/rsc/imports/php/functions/functions.php
@@ -6,8 +6,6 @@ global $con;
 $sql = "SELECT Password FROM volunteer WHERE Email = '$myemail'";
 $result = mysqli_query($con,$sql);
 $row = mysqli_fetch_assoc($result);
-echo $pwhash ,  "|";
-    echo $row['Password'];
 if ($pwhash == $row['Password']) {
 return true;
 } else {
